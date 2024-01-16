6fc1e06dc8166ba1d221a31156049c48649bd230
loveyoun <lovekite0406@naver.com>
2024-01-17 08:24:21 +0900

    feat:edit Spring-DB1 class code

--- jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
+++ jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -1,7 +1,10 @@
 package hello.jdbc.connection;
 
-public abstract class ConnectionConst {
+public abstract class ConnectionConst {  // 객체 생성 안 되게 abstract
+
     public static final String URL = "jdbc:h2:tcp://localhost/~/test";
     public static final String USERNAME = "sa";
     public static final String PASSWORD = "";
+
+
 }
