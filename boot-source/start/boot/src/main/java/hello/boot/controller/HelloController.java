45fb0b299bef75475447c1f5a481a00e269b8013
loveyoun <lovekite0406@gmail.com>
2024-01-29 13:43:39 +0900

    feat: edit Spring-Boot boot, lib project

--- boot-source/start/boot/src/main/java/hello/boot/controller/HelloController.java
+++ boot-source/start/boot/src/main/java/hello/boot/controller/HelloController.java
@@ -1,4 +1,4 @@
-package hello.controller;
+package hello.boot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +8,8 @@ public class HelloController {
 
     @GetMapping("/hello-spring")
     public String hello() {
+        System.out.println("HelloController.hello");
+
         return "hello spring!";
     }
 
